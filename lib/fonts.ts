5bebd445680bafa8ff8f316f3dfede79f80ac522
Masoud Garousian <masoud@Masouds-MacBook-Pro.local>
2026-06-06 12:02:50 +0330

    Use local Vazir font

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -1,4 +1,33 @@
-export const vazirmatn = {
-  variable: "font-sans",
-  className: "font-sans",
-};
+import localFont from "next/font/local";
+
+export const vazirmatn = localFont({
+  src: [
+    {
+      path: "../app/fonts/Vazir-light.ttf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../app/fonts/Vazir.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../app/fonts/Vazir-Medium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../app/fonts/Vazir-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../app/fonts/Vazir-Black.ttf",
+      weight: "900",
+      style: "normal",
+    }
+  ],
+  variable: "--font-vazirmatn",
+  display: "swap",
+});
